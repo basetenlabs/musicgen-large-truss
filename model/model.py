2d04f9e72d6c4118fa5453c413c240a50878a21b
Matt Howard <moward@users.noreply.github.com>
2023-06-21 17:24:26 -0400

    Get output working

--- model/model.py
+++ model/model.py
@@ -1,9 +1,19 @@
 import base64
+import os
 import random
+import tempfile
 
-import torchaudio
 from audiocraft.data.audio import audio_write
 from audiocraft.models import MusicGen
+
+
+def ls_dir(path):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        if os.path.isfile(file_path):
+            # get file size
+            size = os.path.getsize(file_path)
+            print(file_path, size)
 
 
 class Model:
@@ -11,24 +21,25 @@ class Model:
         self.model = MusicGen.get_pretrained("small", device="cuda")
 
     def predict(self, request):
-        with torchaudio.no_grad():
-            try:
-                prompts = request.pop("prompts")
-                duration = request.pop("duration")
-                self.model.set_generation_params(duration=duration)
-                wav = self.model.generate(prompts)
-                filename = hex(random.randint(0, 1000000))[2:]
-                audio_write(
-                    f"{filename}",
-                    wav.cpu(),
-                    self.model.sample_rate,
-                    strategy="loudness",
-                    loudness_compressor=True,
-                )
-                # Read file and encode it into base64 format
-                with open(f"{filename}.wav", "rb") as f:
-                    data = base64.b64encode(f.read()).decode("utf-8")
-                    return {"data": data}
+        try:
+            prompts = request.pop("prompts")
+            duration = request.pop("duration") or 8
+            self.model.set_generation_params(duration=duration)
+            wav = self.model.generate(prompts)
+            output_files = []
+            for idx, one_wav in enumerate(wav):
+                with tempfile.NamedTemporaryFile() as tmpfile:
+                    print(f"Writing {tmpfile.name}")
+                    audio_write(
+                        tmpfile.name,
+                        one_wav.cpu(),
+                        self.model.sample_rate,
+                        strategy="loudness",
+                    )
+                    ls_dir("/tmp")
+                    with open(tmpfile.name + ".wav", "rb") as f:
+                        output_files.append(base64.b64encode(f.read()).decode("utf-8"))
+                return {"data": output_files}
 
-            except Exception as exc:
-                return {"status": "error", "data": None, "message": str(exc)}
+        except Exception as exc:
+            return {"status": "error", "data": None, "message": str(exc)}
