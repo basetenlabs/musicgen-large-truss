0004b5701593e006944038a6b842287c13b08ed0
Matt Howard <moward@users.noreply.github.com>
2023-06-22 11:16:35 -0400

    Clean up model.py

--- model/model.py
+++ model/model.py
@@ -1,24 +1,13 @@
 import base64
-import os
-import random
 import tempfile
 
 from audiocraft.data.audio import audio_write
 from audiocraft.models import MusicGen
 
 
-def ls_dir(path):
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        if os.path.isfile(file_path):
-            # get file size
-            size = os.path.getsize(file_path)
-            print(file_path, size)
-
-
 class Model:
     def load(self):
-        self.model = MusicGen.get_pretrained("small", device="cuda")
+        self.model = MusicGen.get_pretrained("large", device="cuda")
 
     def predict(self, request):
         try:
@@ -29,17 +18,20 @@ class Model:
             output_files = []
             for idx, one_wav in enumerate(wav):
                 with tempfile.NamedTemporaryFile() as tmpfile:
-                    print(f"Writing {tmpfile.name}")
                     audio_write(
                         tmpfile.name,
                         one_wav.cpu(),
                         self.model.sample_rate,
                         strategy="loudness",
                     )
-                    ls_dir("/tmp")
                     with open(tmpfile.name + ".wav", "rb") as f:
                         output_files.append(base64.b64encode(f.read()).decode("utf-8"))
-                return {"data": output_files}
 
+            return {"data": output_files}
         except Exception as exc:
-            return {"status": "error", "data": None, "message": str(exc)}
+            return {
+                "status": "error",
+                "data": None,
+                "message": str(exc),
+                "traceback": str(exc.__traceback__),
+            }
